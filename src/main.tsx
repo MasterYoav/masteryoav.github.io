b6223bcfb7e4388c2f36df8ef0a004bb1fc457ab
MasterYoav <realyoavperetz@gmail.com>
2025-08-22 09:56:05 +0300

    build commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
